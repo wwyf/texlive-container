39e25e3fb0ac36847f8432fc9bbd955d08612417
wwyf <w.y.ff@163.com>
2021-05-15 22:11:57 +0800

    update

--- setup-fonts.sh
+++ setup-fonts.sh
@@ -3,6 +3,6 @@
 apt-get update
 apt-get install -y git language-pack-zh-hans language-pack-zh-hant xfonts-wqy 
 
-echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | sudo debconf-set-selections
+echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections
 apt-get -y install msttcorefonts
 apt-get -y install ttf-mscorefonts-installer
