ae1d7682e1eda4114a4ef72fc13bf2d0151c892f
wwyf <w.y.ff@163.com>
2021-05-16 09:16:47 +0800

    update

--- setup-fonts.sh
+++ setup-fonts.sh
@@ -6,8 +6,8 @@ export DEBIAN_FRONTEND="noninteractive"
 export TZ="Europe/London"
 
 
-apt-get install language-pack-zh*
-apt-get install chinese*
+apt-get install -y language-pack-zh*
+apt-get install -y chinese*
 apt-get install -y git language-pack-zh-hans language-pack-zh-hant xfonts-wqy 
 echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections
 apt-get -y install msttcorefonts
