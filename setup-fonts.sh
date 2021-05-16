156140119e1da4dc835f41e751b1743f916c2feb
wwyf <w.y.ff@163.com>
2021-05-16 09:14:06 +0800

    update

--- setup-fonts.sh
+++ setup-fonts.sh
@@ -5,10 +5,12 @@ apt-get update
 export DEBIAN_FRONTEND="noninteractive"
 export TZ="Europe/London"
 
-apt-get install -y git language-pack-zh-hans language-pack-zh-hant xfonts-wqy 
+
+apt-get install language-pack-zh*
+apt-get install chinese*
 
 
-
-echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections
-apt-get -y install msttcorefonts
-apt-get -y install ttf-mscorefonts-installer
+# apt-get install -y git language-pack-zh-hans language-pack-zh-hant xfonts-wqy 
+# echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections
+# apt-get -y install msttcorefonts
+# apt-get -y install ttf-mscorefonts-installer
