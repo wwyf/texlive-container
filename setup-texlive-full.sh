01a8a790745b6816316cef18031a58d05240e7e8
wwyf <w.y.ff@163.com>
2021-05-16 09:01:14 +0800

    update

--- setup-texlive-full.sh
+++ setup-texlive-full.sh
@@ -6,6 +6,6 @@ tar xf install-tl-unx.tar.gz
 cd install-tl-20210515
 
 
-printf "I\n" ./install-tl  -no-gui 
+printf "I\n" | ./install-tl  -no-gui 
 # ./install-tl  -no-gui --location https://mirrors.matrix.moe/CTAN/systems/texlive/tlnet/
 # 输入I，开始安装
