0c8e0ddbaada404b40bdd5a050448fdd4ba2a9e2
wwyf <w.y.ff@163.com>
2021-05-16 12:39:20 +0800

    add fonts fonts-noto-jck

--- setup-fonts.sh
+++ setup-fonts.sh
@@ -12,6 +12,7 @@ apt-get install -y git language-pack-zh-hans language-pack-zh-hant xfonts-wqy
 echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections
 apt-get -y install msttcorefonts
 apt-get -y install ttf-mscorefonts-installer
+apt-get -y install fonts-noto-cjk
 
 # install adobe fonts
 mkdir -p adobefonts
