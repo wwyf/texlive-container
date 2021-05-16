55e0305a41309f114265638cbc4b771f9684d945
wwyf <w.y.ff@163.com>
2021-05-16 09:23:42 +0800

    update

--- setup-fonts.sh
+++ setup-fonts.sh
@@ -12,3 +12,20 @@ apt-get install -y git language-pack-zh-hans language-pack-zh-hant xfonts-wqy
 echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections
 apt-get -y install msttcorefonts
 apt-get -y install ttf-mscorefonts-installer
+
+# install adobe fonts
+mkdir -p adobefonts
+cd adobefonts
+wget https://cs.fit.edu/code/projects/ndworld/repository/revisions/12/raw/Resources/Fonts/AdobeSongStd-Light.otf
+wget https://cs.fit.edu/code/projects/ndworld/repository/revisions/12/raw/Resources/Fonts/AdobeHeitiStd-Regular.otf
+wget https://cs.fit.edu/code/projects/ndworld/repository/revisions/12/raw/Resources/Fonts/AdobeKaitiStd-Regular.otf
+wget https://cs.fit.edu/code/projects/ndworld/repository/revisions/11/raw/Resources/Fonts/AdobeFangsongStd-Regular.otf
+cd ..
+mv adobefonts /usr/share/fonts/
+cd /usr/share/fonts/adobefonts
+mkfontscale
+mkfontdir
+fc-cache -fv
+
+# fonts lists
+fc-list
